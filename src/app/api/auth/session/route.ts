bcdb065ed54b4f49fc7d61f1a339eb4fef39129e
Roopiee <bhukya.k@netconnectglobal.com>
2026-03-31 13:51:57 +0530

    Few changes

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'MySuperSecretCode';
 const COOKIE_NAME = 'skillkendra_session';
