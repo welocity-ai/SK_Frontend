7f9b6b7e0a907a9cb5fbdda47a06cada23037513
Roopiee <bhukya.k@netconnectglobal.com>
2026-04-13 17:23:23 +0530

    Middleware changed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+  secretKey: process.env.CLERK_SECRET_KEY,
+})
 
 export const config = {
   matcher: [
